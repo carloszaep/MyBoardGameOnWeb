8744dbe60cb355207d20959555bf880d50a63b0c
carloszaep <carlos.paez94@yahoo.com>
2022-07-14 17:57:00 -0400

    adding rolling on property making work

--- script.js
+++ script.js
@@ -22,6 +22,40 @@ class player {
     this.finalTurn = finalTurn;
   }
 }
+
+class property {
+  constructor(
+    landValueSouth,
+    landValueNorth,
+    upgradeSouth1,
+    upgradeSouth2,
+    upgradeSouth3,
+    upgradeNorth1,
+    upgradeNorth2,
+    upgradeNorth3,
+    mapPositionSouth,
+    mapPositionNorth,
+    numOfUpSouth,
+    numOfUpNorth,
+    nameImg,
+    nameImgRes
+  ) {
+    this.nameImg = nameImg;
+    this.landValueSouth = landValueSouth;
+    this.landValueNorth = landValueNorth;
+    this.upgradeNorth1 = upgradeNorth1;
+    this.upgradeNorth2 = upgradeNorth2;
+    this.upgradeNorth3 = upgradeNorth3;
+    this.upgradeSouth1 = upgradeSouth1;
+    this.upgradeSouth2 = upgradeSouth2;
+    this.upgradeSouth3 = upgradeSouth3;
+    this.mapPositionSouth = mapPositionSouth;
+    this.mapPositionNorth = mapPositionNorth;
+    this.numOfUpSouth = numOfUpSouth;
+    this.numOfUpNorth = numOfUpNorth;
+    this.nameImgRes = nameImgRes;
+  }
+}
 //****************assigning classes *************
 
 //fmi count as a player
@@ -51,12 +85,47 @@ const player2 = new player(
   false
 );
 
+// property
+const azucar = new property(
+  100,
+  200,
+  150,
+  200,
+  300,
+  300,
+  400,
+  600,
+  1,
+  21,
+  0,
+  0,
+  "azucar.png",
+  "azucar_res.png"
+);
+// const banano = new property(150, 300, 250, 300, 450, 500, 600, 900, 2, 22, 0, 0, 'banano.png', 'banano_res.png')
+// const cacao = new property(200, 400, 300, 400, 600, 600, 800, 1200, 3, 23, 0, 0, 'cacao.png','cacao_res.png')
+// const algodon = new property(250, 500, 350, 500, 750, 750, 1000, 1500, 5, 25, 0, 0, 'algodon.png')
+// const tabaco = new property(300, 600, 450, 600, 900, 900, 1200, 1800, 6, 26, 0, 0, 'tabaco.png')
+// const cafe = new property(350, 700, 500, 700, 1000, 1000, 1400, 2100, 7, 27, 0, 0, 'cafe.png')
+// const pesca = new property(400, 800, 600, 800, 1200, 1200, 1600, 2400, 9, 29, 0, 0, 'pesca.png')
+// const ganado = new property(500, 1000, 750, 1000, 1500, 1500, 2000, 3000, 11, 31, 0, 0, 'ganado.png')
+// const cobre = new property(600, 1200, 900, 1200, 1800, 1800, 2400, 3600, 13, 33, 0, 0, 'cobre.png')
+// const estano = new property(700, 1400, 1050, 1400, 2100, 2100, 2800, 4200, 14, 34, 0, 0, 'estano.png')
+// const hierro = new property(800, 1600, 1200, 1600, 2400, 2400, 3200, 4800, 15, 35, 0, 0, 'hierro.png')
+// const petroleo = new property(1200, 2400, 1800, 2400, 3600, 3600, 4800, 7200, 17, 37, 0, 0, 'petroleo.png')
+
 // ***********defining html element**********
-// buttom tirar dado
+// buttoms
 const BtnRollDice = document.querySelector(".btnRollDice");
+const btnUpgradeIndustry = document.querySelector(".btnUpgradeIndustry");
+const btnBuy = document.querySelector(".btnBuy");
 // dices img
 const diceImg = document.querySelector(".diceImg");
 const diceImg2 = document.querySelector(".diceImg2");
+// on show
+const onShow = document.querySelector(".onshow");
+// overlay
+const overlay = document.querySelector(".overlay");
 
 //*******game logic variables*********
 // knowing what player is active by number
@@ -64,6 +133,12 @@ let activePlayer = 1; // i need to make a function so the 1 chnage depending of 
 
 // list all player fmi is already on
 const players = [fmi];
+// list all properties
+const properties = [azucar];
+// adding all properties to fmi propertyOn
+for (i of properties) {
+  fmi.propertyOn.push(i);
+}
 // number of player is going to be define lether by the user
 let numberOfPlayers = 2;
 
@@ -77,8 +152,8 @@ if (numberOfPlayers === 2) {
 // when moving
 const movePlayer = function (player) {
   // dices
-  let dice = Math.floor(Math.random() * 6) + 1;
-  let dice2 = Math.floor(Math.random() * 6) + 1;
+  let dice = 1; //Math.floor(Math.random() * 6) + 1;
+  let dice2 = 0; // Math.floor(Math.random() * 6) + 1;
   // chnage the dice img by dice value
   diceImg.src = `../tablero/dice-${dice}.png`;
   diceImg2.src = `../tablero/dice-${dice2}.png`;
@@ -119,21 +194,36 @@ const switchActivePlayer = function () {
   }
 };
 
+// rolling on property
+const rollingOnProperty = function () {
+  for (let i = 0; i < properties.length; i++)
+    if (
+      players[activePlayer].mapPosition === fmi.propertyOn[i].mapPositionSouth
+    ) {
+      overlay.classList.remove("hidden");
+      onShow.src = `../propiedades/${fmi.propertyOn[i].nameImg}`;
+      onShow.classList.remove("hidden");
+      btnBuy.classList.remove("hidden");
+    }
+};
+
 //**********functions to put in a event listener*************
 // roll dice
 const rollDice = function () {
-  for (let i = 0; i < numberOfPlayers; i++) {
-    if (players[activePlayer]) {
-      // all options that happen in the star of the turn when roll a dice
-      if (players[activePlayer].startTurn) {
-        movePlayer(players[activePlayer]);
-        switchStarTurn();
-        // all options that happen when is final turn
-      } else if (players[activePlayer].finalTurn) {
-        switchActivePlayer();
-      }
-    }
+  // for (let i = 0; i < numberOfPlayers; i++) {
+  //   if (players[activePlayer]) {
+  // all options that happen in the star of the turn when roll a dice
+  if (players[activePlayer].startTurn) {
+    movePlayer(players[activePlayer]);
+    switchStarTurn();
+    // all options that happen when is final turn
   }
+  if (players[activePlayer].finalTurn) {
+    rollingOnProperty();
+    switchActivePlayer();
+  }
+  // }
+  //
 };
 
 // ************buttoms**********
